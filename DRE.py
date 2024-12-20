171a9247e58e6d28f15cb9551ee8712b5f92b25e
Alexarcelo <marcelo@luckreceptivo.com.br>
2024-12-20 14:59:43 -0300

    Update DRE.py

--- DRE.py
+++ DRE.py
@@ -355,10 +355,15 @@ def grafico_linha_percentual(referencia, eixo_x, eixo_y_1, ref_1_label, titulo):
 
 def puxar_aba_simples(id_gsheet, nome_aba, nome_df):
 
-    nome_credencial = st.secrets["CREDENCIAL_SHEETS"]
-    credentials = service_account.Credentials.from_service_account_info(nome_credencial)
-    scope = ['https://www.googleapis.com/auth/spreadsheets']
-    credentials = credentials.with_scopes(scope)
+    project_id = "grupoluck"
+    secret_id = "cred-luck-aracaju"
+    secret_client = secretmanager.SecretManagerServiceClient()
+    secret_name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
+    response = secret_client.access_secret_version(request={"name": secret_name})
+    secret_payload = response.payload.data.decode("UTF-8")
+    credentials_info = json.loads(secret_payload)
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    credentials = Credentials.from_service_account_info(credentials_info, scopes=scopes)
     client = gspread.authorize(credentials)
 
     spreadsheet = client.open_by_key(id_gsheet)
