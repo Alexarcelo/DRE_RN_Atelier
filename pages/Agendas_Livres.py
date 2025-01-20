eda7d788cd7a2103fd9cdbcefa57a7dab5b95a4c
Alexarcelo <marcelo@luckreceptivo.com.br>
2025-01-20 17:22:23 -0300

    Update Agendas_Livres.py

--- pages/Agendas_Livres.py
+++ pages/Agendas_Livres.py
@@ -205,6 +205,53 @@ if data_inicial and data_final and len(colaboradores) > 0:
 
     container_dataframe.dataframe(styled_df, hide_index=True, use_container_width=True, height=1000)
 
+elif data_inicial and len(colaboradores) > 0:
+
+    data_final = st.session_state.df_agenda_producao['Data da Atividade'].max()
+
+    df_agendas_livres = gerar_df_agendas_livres(data_inicial, data_final)
+
+    for colaborador in colaboradores:
+
+        df_colaborador = st.session_state.df_agenda_producao[st.session_state.df_agenda_producao['Colaborador'] == colaborador].reset_index(drop=True)
+
+        df_colaborador = df_colaborador.groupby('Data da Atividade')['Colaborador'].count().reset_index()
+
+        df_agendas_livres = pd.merge(df_agendas_livres, df_colaborador, on='Data da Atividade', how='left')
+
+        df_agendas_livres = df_agendas_livres.rename(columns={'Colaborador': colaborador})
+
+        df_agendas_livres[colaborador] = df_agendas_livres[colaborador].fillna('Livre')
+
+        if apenas_livres and len(apenas_livres) > 0:
+
+            df_agendas_livres = df_agendas_livres[df_agendas_livres[colaborador] == 'Livre']
+
+        for coluna in df_agendas_livres.columns:
+
+            if coluna!='Data da Atividade':
+
+                df_agendas_livres[coluna] = df_agendas_livres[coluna].apply(lambda x: str(x)[:1] if x!='Livre' else x)
+
+    df_agendas_livres['Dia da Semana'] = pd.to_datetime(df_agendas_livres['Data da Atividade']).dt.day_name()
+    df_agendas_livres['Dia da Semana'] = df_agendas_livres['Dia da Semana'].map(dias_da_semana)
+
+    lista_colunas = ['Data da Atividade', 'Dia da Semana']
+
+    lista_colunas.extend(colaboradores)
+
+    df_agendas_livres = df_agendas_livres[lista_colunas]
+
+    if len(filtrar_dias_da_semana)>0:
+
+        df_agendas_livres = df_agendas_livres[~df_agendas_livres['Dia da Semana'].isin(filtrar_dias_da_semana)]
+
+    styled_df = df_agendas_livres.style.applymap(style_livre)
+
+    container_dataframe = st.container()
+
+    container_dataframe.dataframe(styled_df, hide_index=True, use_container_width=True, height=1000)
+
 elif len(colaboradores) > 0:
 
     data_inicial = st.session_state.df_agenda_producao['Data da Atividade'].min()
